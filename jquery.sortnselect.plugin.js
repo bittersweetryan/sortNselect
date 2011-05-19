a7932a21085f8f68786301797f9d931219806069
Ryan Anklam <ryan.anklam@gmail.com>
2011-05-18 19:16:25 -0500

    abstracted out sortable click function and added css to the ul

--- jquery.sortnselect.plugin.js
+++ jquery.sortnselect.plugin.js
@@ -1,10 +1,35 @@
+/*
+Copyright (c) 2011 Ryan Anklam
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
 (function($){
 	$.fn.extend({
 		sortnselect: function(){
 		
 			var target = this;
 			var checkOK = '<span class="ui-icon ui-icon-circle-check"></span>';
-
+			var handle = '<span class="ui-icon ui-icon-grip-dotted-vertical"></span>';
+			
+			target.children().prepend(handle);
+			target.children().addClass("ui-state-default ui-corner-all");
+	
 			target.sortable({
 					placeholder: "ui-state-highlight",
 					cursor: "hand",
@@ -16,40 +41,44 @@
 							//$.reorderList
 					}
 			}).children().click(function(event){
-		
-				if(!$(this).parent().hasClass("noclick")){
-					if(!$(this).hasClass("ui-state-hover")){
+				sortableClick($(this),event);
 				
-						$(this).addClass("ui-state-hover");
-						$(this).children(":checkbox").attr("checked","checked");
+			}).children(":checkbox").offset({top:0,left:-999}).end().css( 'cursor', 'pointer').mousedown(function(){
+				$(this).css('cursor','move');
+			}).mouseup(function(){
+				$(this).css('cursor','pointer');
+			});
+	
+			target.disableSelection();
+			
+			sortableClick = function(element, event){
+				if(!element.parent().hasClass("noclick")){
+					if(!element.hasClass("ui-state-hover")){
 				
-						if($(this).children(".ui-icon-circle-check").length === 0){
-							$(this).append(checkOK);	
+						element.addClass("ui-state-hover");
+						element.children(":checkbox").attr("checked","checked");
+				
+						if(element.children(".ui-icon-circle-check").length === 0){
+							element.append(checkOK);	
 						}				
 					}
 					else{
 				
-						$(this).removeClass("ui-state-hover");
-						$(this).children(":checkbox").removeAttr('checked');
+						element.removeClass("ui-state-hover");
+						element.children(":checkbox").removeAttr('checked');
 				
-						$(this).children(".ui-icon-circle-check").remove();
+						element.children(".ui-icon-circle-check").remove();
 					}
 			
-					$.reorderList($(this),$(this).parent(),event);
-					$.setShortlist($(this).parent(),event);
+					$.reorderList(element,element.parent(),event);
+					$.setShortlist(element.parent(),event);
 			
 					event.preventDefault();
 				}
 				else{
-					$(this).parent().removeClass("noclick");
+					element.parent().removeClass("noclick");
 				}
-				}).children(":checkbox").offset({top:0,left:-999}).end().css( 'cursor', 'pointer').mousedown(function(){
-					$(this).css('cursor','move');
-				}).mouseup(function(){
-					$(this).css('cursor','pointer');
-				});
-	
-			target.disableSelection();
+			}
 		
 			$.reorderList = function(theTarget,theList,event){
 	
@@ -61,8 +90,10 @@
 				else{
 					theList.prepend(theTarget);
 				}
-			}
-	
+			};
+			
+			
+			
 			$.setShortlist = function(theList,event){
 				var shortlistHiddenInput = $("#shortlist");
 				var shortlistVendors = "";
@@ -74,8 +105,7 @@
 				shortlistHiddenInput.val(shortlistVendors);
 	
 				console.log(shortlistHiddenInput.val());
-			}	
-		
+			};	
 		}
 	})
 })(jQuery);
